b9f99e2f7bd4af17dec66353d3b3156c8e91ce85
TouchySpidey <cesca.leonardo@gmail.com>
2023-09-09 10:08:08 +0200

    firebase auth

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express = require('express')
+const authenticate = require('./authMiddleware');
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => {
+// firebase
+const firebaseAdmin = require('firebase-admin');
+firebaseAdmin.initializeApp();
+
+app.get('/version', (req, res) => {
     // get app version from package.json
     const appVersion = require('./package.json').version
-    // set status code to 200 (OK)
-    res.status(200)
-    // out app version
-    res.send(`Hello World 😍! v${appVersion}`)
+    const appStuff = firebaseAdmin.app()
+    // out app name and version
+    res.send(`Running ${appStuff.name} on v${appVersion} 😍`)
 })
+
+app.get('/user/info', authenticate, (req, res) => {
+    // At this point, the user is authenticated, and their UID is in req.uid
+    res.send(`User logged in.  Here's their email as proof: ${req.email}`);
+});
+
+// this is for debugging purposes only
+app.get('/test.html', (req, res) => {
+    res.sendFile(__dirname + '/test.html');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
---
+++ authMiddleware.js
@@ -0,0 +1,23 @@
+const firebaseAdmin = require('firebase-admin');
+
+const authenticate = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).send('Unauthorized');
+  }
+
+  const idToken = authHeader.split('Bearer ')[1];
+
+  try {
+    const decodedToken = await firebaseAdmin.auth().verifyIdToken(idToken);
+    req.uid = decodedToken.uid;
+    req.email = decodedToken.email;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).send('Invalid token');
+  }
+};
+
+module.exports = authenticate;
