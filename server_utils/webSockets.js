c4a2e31ae0af2f4390c58d6428abdd2a6edee76c
TouchySpidey <cesca.leonardo@gmail.com>
2023-10-21 17:00:54 +0200

    fix, no need for keepalive web socket handling

--- server_utils/webSockets.js
+++ server_utils/webSockets.js
@@ -6,67 +6,35 @@ global.userSockets = {};
 module.exports = (socketIo) => {
     try {
         socketIo.on('connection', async (socket) => {
+            const userData = await token_verifier(socket.handshake.query.token);
+            const { firebaseUID } = userData;
+            const userUID = firebaseUID ? await getUserUIDFromFirebaseUID(firebaseUID) : null;
+            const socketUID = userUID ? uuidv4() : null;
+
             socket.on('disconnect', () => {
                 console.log('Client disconnected');
-            });
-    
-            const userData = await token_verifier(socket.handshake.query.token);
+                if (userUID in global.userSockets) {
+                    if (socketUID in global.userSockets[userUID]) {
+                        delete global.userSockets[userUID][socketUID];
+                        if (Object.keys(global.userSockets[userUID]).length === 0) delete global.userSockets[userUID];
+                    }
+                }
+                console.log(global.userSockets);
+            })
             
-            if (!userData) {
-                return socket.disconnect(true);
-            }
-            console.log(userData);
-            
-            const { firebaseUID, email } = userData;
-            console.log('New client connected', email);
-            const userUID = await getUserUIDFromFirebaseUID(firebaseUID);
-            console.log(firebaseUID, userUID);
             if (!userUID) {
-                return socket.disconnect(true);
+                return socket.disconnect(true);;
             }
         
-            const socketUID = uuidv4();
             if (!(userUID in global.userSockets)) {
                 global.userSockets[userUID] = {};
             }
             global.userSockets[userUID][socketUID] = socket;
             console.log(global.userSockets);
-            
-            // gestione keepalive, per evitare che il server si riempia di socket che sono disconnessi
-            let actualKiller = null;
-            const killingCheck = setInterval(() => {
-                // ogni 5 minuti richiedi al client di mandare un messaggio
-                // se non arriva entro 30 secondi, disconnetti il client
-                actualKiller = setTimeout(_ => killSocket(killingCheck, userUID, socketUID, socket), 1000 * 3);
-                socket.emit('keepalive');
-                console.log('keepalive request sent')
-            }, 1000 * 1 * 8);
-            
-            socket.on('keepalive', async (accessToken) => {
-                console.log('keepalive response received');
-                const checkRefresh = await token_verifier(accessToken);
-                if (checkRefresh) {
-                    clearTimeout(actualKiller);
-                } else {
-                    // invalid token, same as if the client didn't send keepalive
-                    killSocket(killingCheck, userUID, socketUID, socket)
-                }
-            });
         });
     } catch (e) {
         console.log(e);
     }
-}
-
-function killSocket(killingCheck, userUID, socketUID, socket) {
-    console.log('Killing socket', userUID, socketUID);
-    clearInterval(killingCheck);
-    socket.disconnect(true);
-    delete global.userSockets[userUID][socketUID];
-    if (Object.keys(global.userSockets[userUID]).length === 0) {
-        delete global.userSockets[userUID];
-    }
-    console.log(global.userSockets);
 }
 
 global.sendSocketMessage = (userUID, type, body) => {
@@ -80,7 +48,10 @@ global.sendSocketMessage = (userUID, type, body) => {
     }
 }
 
-async function getUserUIDFromFirebaseUID(firebaseUID) {
+async function getUserUIDFromFirebaseUID(firebaseUID = null) {
+    if (!firebaseUID) {
+        return false;
+    }
     console.log(firebaseUID);
     const [rows] = await global.db.execute('SELECT * FROM users WHERE firebaseUid = ?', [firebaseUID]);
     if (rows.length === 0) {
